dc9afa8205edcb138d14fe79b91c09301bde0eba
Samarendra Gouda <samarendragouda@gmail.com>
2023-12-25 23:50:19 +0530

    feat: create-todos

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,8 @@
 use crate::model::{AppState, QueryOptions, Todo};
 use crate::response::{GenericResponse, TodoListResponse, TodoResponse};
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
+use chrono::prelude::*;
+use uuid::Uuid;
 
 #[get("/test")]
 async fn test_handler() -> impl Responder {
@@ -34,9 +36,47 @@ pub async fn todos_list_handler(
     HttpResponse::Ok().json(json_response)
 }
 
+#[post("/todos")]
+async fn create_todo_handler(
+    mut body: web::Json<Todo>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let mut vec = data.todo_db.lock().unwrap();
+
+    let todo = vec.iter().find(|todo| todo.title == body.title);
+
+    if todo.is_some() {
+        let error_response = GenericResponse {
+            status: "fail".to_string(),
+            message: format!("Todo with title: '{}' already exists", body.title),
+        };
+        return HttpResponse::Conflict().json(error_response);
+    }
+
+    let uuid_id = Uuid::new_v4();
+    let datetime = Utc::now();
+
+    body.id = Some(uuid_id.to_string());
+    body.completed = Some(false);
+    body.createdAt = Some(datetime);
+    body.updatedAt = Some(datetime);
+
+    let todo = body.to_owned();
+
+    vec.push(body.into_inner());
+
+    let json_response = TodoResponse {
+        status: "success".to_string(),
+        data: { todo },
+    };
+
+    HttpResponse::Ok().json(json_response)
+}
+
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/api")
         .service(test_handler)
+        .service(create_todo_handler)
         .service(todos_list_handler);
 
     conf.service(scope);
